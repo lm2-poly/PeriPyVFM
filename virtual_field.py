d198987e0e2b52b8718a7773663ca241921b4bc3
Patrick Diehl <patrick.diehl@polymtl.ca>
2017-06-21 15:41:48 -0400

    Backup f * u

--- virtual_field.py
+++ virtual_field.py
@@ -5,7 +5,7 @@ import random
 from scipy.optimize import minimize, fmin_cobyla
 import sys
 
-case = ""
+case = "sym"
 
 def readVirtualField(filename):
     
@@ -20,8 +20,8 @@ def readVirtualField(filename):
     
     
     for i in range(0,len(data)):
-        u[i][0] = data[i][4] + random.uniform(-1.,1.) * 1e-4
-        u[i][1] = data[i][5] + random.uniform(-1.,1.) * 1e-3
+        u[i][0] = data[i][4] 
+        u[i][1] = data[i][5] 
         
     return u
 
@@ -38,19 +38,19 @@ def res1(vf1,vf2):
  
 def res1_sym(vf1,vf2):
      vf1 += 50000
-     return np.sqrt(vf1*vf1 + vf2*vf2)
+     return np.sqrt(vf1*vf1  + vf2*vf2 )
  
 def res2(vf1,vf2):
     return np.sqrt(vf1*vf1 + vf2*vf2) / 56000.
 
 def res2_sym(vf1,vf2):
-    return np.sqrt(vf1*vf1 + vf2*vf2) / 50000.
+    return np.sqrt(vf1*vf1 + vf2*vf2 ) / 50000.
 
 
 def residual(P, deck):
     
-    #deck.bulk_modulus = P[0]
-    #deck.shear_modulus = P[1]
+    deck.bulk_modulus = P[0]
+    deck.shear_modulus = P[1]
    
     problem = DIC_problem(deck)
     vf1 = 0
@@ -58,19 +58,19 @@ def residual(P, deck):
     energy = 0
     for i in range(0,len(u1)):
         #if deck.geometry.nodes[i][1] >= 7.:
-        vf1 += np.dot(problem.force_int[i,:,1] , u1[i]) * deck.geometry.volumes[i]
-        vf2 += np.dot(problem.force_int[i,:,1] , u2[i]) * deck.geometry.volumes[i]   
+        vf1 += np.dot(problem.force_int[i,:,1] , u1[i]) * deck.geometry.volumes[i] 
+        vf2 += np.dot(problem.force_int[i,:,1] , u2[i]) * deck.geometry.volumes[i] 
         energy += problem.strain_energy[i]
-    print "Energies" ,vf1 , vf2 , energy
+    #print "Energies" ,vf1 , vf2 , energy
     
     
     if deck.vtk_writer.vtk_enabled == True:
         writeParaview(deck,problem)
     
     if case == "sym":
-        print deck.bulk_modulus, deck.shear_modulus, res2_sym(vf1,vf2)
-        sys.exit()
-        return res2_sym(vf1,vf2)
+        print deck.bulk_modulus, deck.shear_modulus, res2_sym(vf1,vf2) / (40*70)
+        #sys.exit()
+        return res2_sym(vf1,vf2) / (40*70)
     else:
         print deck.bulk_modulus, deck.shear_modulus, res2(vf1,vf2)
         sys.exit()
@@ -97,7 +97,9 @@ else:
 p = np.array((random.uniform(0.1, 10.) * 1000.,
                   random.uniform(0.1, 10.) * 1000.), dtype=float)
 
-res = minimize(residual, p, args=(deck), method='COBYLA', tol=1e-3,
-                   options={'rhobeg': 100.,'disp': True })
+#res = minimize(residual, p, args=(deck), method='COBYLA', tol=1e-8,
+ #                  options={'rhobeg': 100.,'disp': True })
+ 
+res = minimize(residual, p, args=(deck), method='Nelder-Mead', tol=1e-3)
 
 print res.x
