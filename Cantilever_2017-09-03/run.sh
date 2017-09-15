6683d085dba5a7ccd16278a84a5325c7bfdb8bc0
Patrick Diehl <patrick.diehl@polymtl.ca>
2017-09-15 13:49:39 -0400

    Script to run the samples which were generated with samples.sh

---
+++ Cantilever_2017-09-03/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+filename="csv/nodal_spacing.dat"
+while read -r line
+do
+    spacing="$line"
+    cp csv/mesh_vf2_2.csv_"$spacing" csv/mesh_vf2_2.csv 
+    cp csv/mesh_vf3_2.csv_"$spacing" csv/mesh_vf3_2.csv 
+    cp csv/cantilever_dic_2.csv_"$spacing" csv/cantilever_dic_2.csv 
+    	
+    for i in `seq 3 10`;
+    do
+        cp csv/input_elas_2D.yaml_"$spacing"_$i input_elas_2D.yaml
+     	echo "$spacing" , $i >> result.dat
+     	python virtual_field.py >> result.dat
+    done
+done < "$filename"
+sed -i '/capi_return is NULL/d' result.dat
+sed -i '/Call-back cb_calcfc_in__cobyla__user__routines failed./d' result.dat
+sed -i '/K = 3333.3333 G = 1538.4615/d' result.dat
+
