21f1369c383288ff5481a11eeb0b901fefcb6e06
Patrick Diehl <patrick.diehl@polymtl.ca>
2017-10-24 09:26:35 -0400

    Latest version of the virtual fields

--- Bending/vf2.py
+++ Bending/vf2.py
@@ -10,5 +10,5 @@ with open(sys.argv[1], 'rb') as csvfile:
      for row in spamreader:
          x = float(row[0])
          y = float(row[1])
-         print  str(i)+","+row[0]+","+row[1]+","+row[2]+","+str((-x/64.)*(y/32.))+","+str((1./48.)*(x*x-48.*48.))+",0."
+         print  str(i)+","+row[0]+","+row[1]+","+row[2]+","+str((-x/64.)*(y/32.))+","+str((48.*48.*48.*48.-x*x*x*x)/(48.**4./32.))+",0."
          i += 1
