795af209051ab9416283a81192c5924cc6f31faa
Patrick Diehl <patrick.diehl@polymtl.ca>
2017-12-19 13:13:01 -0500

    Update vfm4

--- Bending/vf4.py
+++ Bending/vf4.py
@@ -11,5 +11,5 @@ with open(sys.argv[1], 'rb') as csvfile:
      i = 0
      print "#id,x,y,z,u,v,w"
      for row in spamreader:
-         print  str(i)+","+row[0]+","+row[1]+","+row[2]+",0.,"+str(-W*(float(row[0])/L)*(float(row[1])/W))+","+str(W*(float(row[0])**4-S2**4)/S2**4)
+         print  str(i)+","+row[0]+","+row[1]+","+row[2]+","+str(-W*(float(row[0])/L)*(float(row[1])/W))+","+str(W*(float(row[0])**4-S2**4)/S2**4)+",0."
          i+=1
