b38ab96c80b328318866a2b13cd01dd9f5d2d595
Patrick Diehl <patrick.diehl@polymtl.ca>
2018-02-12 18:25:21 -0500

    Add comments and update volume

--- run_nomad.sh
+++ run_nomad.sh
@@ -1,6 +1,6 @@
 for i in `seq 1 15`;
    do
-       /calculs/Compile/nomad.3.8.1/bin/nomad param_$i.txt >> result_$i.dat &
+       /calculs/Compile/nomad.3.8.1/bin/nomad param/param_$i.txt >> result_$i.dat &
        echo $i
    done
 
