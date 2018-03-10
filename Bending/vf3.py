6822dfe326c7d60b11dc6b47447e2c666f8a0f68
Rolland Delorme <rolland.delorme@gmail.com>
2018-03-10 13:29:44 -0500

    Virtual field based on the two first solutions of Schrodinger equation

--- Bending/vf3.py
+++ Bending/vf3.py
@@ -6,9 +6,13 @@
 
 import csv
 import sys
+import numpy as np
 
 L = 128.
 W = 32.
+C = 5.
+SigL = (L/2.)/C
+SigW = (W/2.)/C
           
 with open(sys.argv[1], 'rb') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -18,5 +22,6 @@ with open(sys.argv[1], 'rb') as csvfile:
      for row in spamreader:
          x = float(row[0])
          y = float(row[1])
-         print  str(i)+","+row[0]+","+row[1]+","+row[2]+","+str((x*abs(x)*y)/(L*W))+",0.,0."
+         #print  str(i)+","+row[0]+","+row[1]+","+row[2]+","+str((x*abs(x)*y)/(L*W))+",0.,0."
+         print  str(i)+","+row[0]+","+row[1]+","+row[2]+","+str((x*np.exp(-0.5*((x)/SigL)**2.))*(W/2*np.exp(-0.5*((y-W/2.)/SigW)**2.)))+",0.,0."
          i += 1 
