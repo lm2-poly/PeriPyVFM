417882dc1dbc95c0a411aff68bee0c28cfea0dd5
Rolland Delorme <rolland.delorme@gmail.com>
2018-04-17 21:54:25 -0400

    Update vf1-dic.py

--- Bending/vf1-dic.py
+++ Bending/vf1-dic.py
@@ -1,20 +1,26 @@
 # -*- coding: utf-8 -*-
-# Script to generate the first virtual field for the displacement obtained by digital
-# image correlation
+# Script to generate the second virtual field for the displacement obtained by a finite
+# element simulations
 #@author: rolland.delorme@polymtl.ca
 #@author: patrick.diehl@polymtl.ca
-
 import csv
 import sys
+import numpy as np
 
-S = 75.
-
+L = 127.
+W = 31.
+C = 4.5
+SigL = (L/2.)/C
+SigW = (W/2.)/C
+          
 with open(sys.argv[1], 'rb') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
      spamreader.next()
      i = 0
      print "#id,x,y,z,u,v,w"
      for row in spamreader:
-         
-         print  str(i)+","+row[0]+","+row[1]+","+row[2]+",0.,"+str(abs(float(row[0]))-S/2)+",0."
-         i+=1
+         x = float(row[0])
+         y = float(row[1])
+         #print  str(i)+","+row[0]+","+row[1]+","+row[2]+",0.,"+str(abs(x)-S/2)+",0."
+         print  str(i)+","+row[0]+","+row[1]+","+row[2]+","+str(((2./W)*x*np.exp(-0.5*((x)/SigL)**2.))*(L/2*np.exp(-0.5*((y-W/2.)/SigW)**2.)))+","+str((-y/2*np.exp(-0.5*(x/SigL)**2.)))+",0.,0."
+         i += 1  
