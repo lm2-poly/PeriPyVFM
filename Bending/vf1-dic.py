93123361e208aac314edd74714af44ff903512c3
Rolland Delorme <rolland.delorme@gmail.com>
2018-06-09 13:36:37 -0400

    Update vf1-dic.py
    Use of the same field as in the paper and for FEM
    Space between support S=75mm

--- Bending/vf1-dic.py
+++ Bending/vf1-dic.py
@@ -9,6 +9,7 @@ import numpy as np
 
 L = 127.
 W = 31.
+S = 75.
 C = 4.5
 SigL = (L/2.)/C
 SigW = (W/2.)/C
@@ -21,6 +22,6 @@ with open(sys.argv[1], 'rb') as csvfile:
      for row in spamreader:
          x = float(row[0])
          y = float(row[1])
-         #print  str(i)+","+row[0]+","+row[1]+","+row[2]+",0.,"+str(abs(x)-S/2)+",0."
-         print  str(i)+","+row[0]+","+row[1]+","+row[2]+","+str(((2./W)*x*np.exp(-0.5*((x)/SigL)**2.))*(L/2*np.exp(-0.5*((y-W/2.)/SigW)**2.)))+","+str((-y/2*np.exp(-0.5*(x/SigL)**2.)))+",0.,0."
+         print  str(i)+","+row[0]+","+row[1]+","+row[2]+",0.,"+str(abs(x)-S/2)+",0."
+         #print  str(i)+","+row[0]+","+row[1]+","+row[2]+","+str(((2./W)*x*np.exp(-0.5*((x)/SigL)**2.))*(L/2*np.exp(-0.5*((y-W/2.)/SigW)**2.)))+","+str((-y/2*np.exp(-0.5*(x/SigL)**2.)))+",0.,0."
          i += 1  
