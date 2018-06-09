c85b31ecaab7f468343b78740f8b371d6aed878c
Rolland Delorme <rolland.delorme@gmail.com>
2018-06-09 14:00:46 -0400

    Update nomad-dic.py
    vf2 and u2 used to be consistent with the paper.

--- nomad-dic.py
+++ nomad-dic.py
@@ -38,10 +38,10 @@ def res1(Wint_vf1,Wint_vf2,Wint_vf3,Wint_vf4):
      S = 75.
      W = 31.
      Wext_vf1 = F*W/2.  
-     Wext_vf3 = -F*W/2
-     Wext_vf2 = 0.
+     Wext_vf2 = (-F*W/2)*0
+     Wext_vf3 = 0.
      Wext_vf4 = 0.
-     print Wint_vf1 , Wint_vf3
+     print Wint_vf1 , Wint_vf2 , Wint_vf3 , Wint_vf4
      return np.sqrt((Wint_vf1+Wext_vf1)**2 + (Wint_vf2+Wext_vf2)**2 + (Wint_vf3+Wext_vf3)**2 + (Wint_vf4+Wext_vf4)**2) / np.sqrt(Wext_vf1**2 + Wext_vf2**2 + Wext_vf3**2 + Wext_vf4**2)
  
 
@@ -58,19 +58,19 @@ def residual(P, deck):
     writeParaview(deck,problem) 
     for i in range(0,len(u1)):
         Wintvf1 += np.dot(problem.force_int[i,:,1] , u1[i]) * deck.geometry.volumes[i] 
-        #Wintvf2 += np.dot(problem.force_int[i,:,1] , u2[i]) * deck.geometry.volumes[i] 
-        Wintvf3 += np.dot(problem.force_int[i,:,1] , u3[i]) * deck.geometry.volumes[i]
+        Wintvf2 += np.dot(problem.force_int[i,:,1] , u2[i]) * deck.geometry.volumes[i] 
+        #Wintvf3 += np.dot(problem.force_int[i,:,1] , u3[i]) * deck.geometry.volumes[i]
         #Wintvf4 += np.dot(problem.force_int[i,:,1] , u4[i]) * deck.geometry.volumes[i]
     return res1(Wintvf1,Wintvf2,Wintvf3,Wintvf4)
     
     
     
-bnds=((0.1,10000),(0.1,10000))   
+bnds=((500.,5000.),(500.,5000.))   
 
 
 u1 = readVirtualField("./Bending/mesh_vf1_dic.csv")
-#u2 = readVirtualField("./Bending/mesh_vf2_0_25.csv")
-u3 = readVirtualField("./Bending/mesh_vf3_dic.csv")
+u2 = readVirtualField("./Bending/mesh_vf2_0_25.csv")
+#u3 = readVirtualField("./Bending/mesh_vf3_dic.csv")
 #u4 = readVirtualField("./Bending/mesh_vf4_0_25.csv")
 
 deck = DIC_deck("./input_elas_2D-dic.yaml")
